7c29f71a99bebac002070c58650b6174171923c0
STACiA <69411527+staciax@users.noreply.github.com>
2024-08-09 19:20:04 +0700

    implement login page

---
+++ frontend/lib/elma/actions/signin.ts
@@ -0,0 +1,21 @@
+'use server';
+
+import axios from '@/lib/axios';
+
+type Token = {
+	accessToken: string;
+};
+
+export const signin = async (email: string, password: string): Promise<Token> => {
+	console.log(`signin: ${email}, ${password}`);
+	try {
+		const response = await axios.post('/v1/auth/sign', {
+			// TODO: remove hard-coded values
+			email: 'test@test.com',
+			password: 'testtest',
+		});
+		return response.data;
+	} catch (error) {
+		return Promise.reject(error);
+	}
+};
