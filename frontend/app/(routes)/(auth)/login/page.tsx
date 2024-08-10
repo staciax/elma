3a9e5fd19d1be886c554b1f26dde52bc177e45a0
STACiA <69411527+staciax@users.noreply.github.com>
2024-08-10 16:55:40 +0700

    init shadcn/ui

--- frontend/app/(routes)/(auth)/login/page.tsx
+++ frontend/app/(routes)/(auth)/login/page.tsx
@@ -1,9 +1,11 @@
 import SignInForm from '@/components/forms/sign-in';
+import { Button } from '@/components/ui/button';
 
 async function Page() {
 	return (
 		<div>
 			<SignInForm />
+			<Button>Sign in</Button>
 		</div>
 	);
 }
