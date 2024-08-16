2b380a6b1bceba7a3b4202cc49d9518b198bde27
STACiA <69411527+staciax@users.noreply.github.com>
2024-08-16 13:48:10 +0700

    update next, tailwindcss config

---
+++ frontend/next.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+import path from 'path';
+const nextConfig = {
+	// output: 'standalone',
+	webpack: (config) => {
+		config.resolve.alias['@'] = path.resolve();
+		return config;
+	},
+	// eslint: {
+	// 	// Warning: This allows production builds to successfully complete even if
+	// 	// your project has ESLint errors.
+	// 	ignoreDuringBuilds: true,
+	// },
+};
+
+export default nextConfig;
