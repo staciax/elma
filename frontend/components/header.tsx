8aca91017324d603dc561c022b1de1d3ca7702a6
STACiA <69411527+staciax@users.noreply.github.com>
2024-08-16 13:49:31 +0700

    [frontend] update header,

--- frontend/components/header.tsx
+++ frontend/components/header.tsx
@@ -1,5 +1,17 @@
 function Header() {
-	return <div>Header</div>;
+	return (
+		<header>
+			<div className="container mx-auto flex flex-row items-center justify-between ">
+				<div>
+					<span className=" text-2xl">STACIA.</span>
+				</div>
+				<div>
+					<input type="text" className="indent-2" />
+				</div>
+				<div>auth</div>
+			</div>
+		</header>
+	);
 }
 
 export default Header;
