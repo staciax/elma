1228948263cadc383ebe10e6eaf9d7c590fbc9a2
STACiA <69411527+staciax@users.noreply.github.com>
2024-08-18 22:29:19 +0700

    clean

--- backend2/src/api/index.ts
+++
@@ -1,5 +0,0 @@
-import { Elysia } from "elysia";
-
-import { router as sign_router } from "./routes/sign";
-
-export const router = new Elysia({ prefix: "/api" }).use(sign_router);
--- backend2/src/app.ts
+++
@@ -1,6 +0,0 @@
-import cors from '@elysiajs/cors';
-import swagger from '@elysiajs/swagger';
-import { Elysia } from 'elysia';
-import { router as api_router } from './api';
-
-export const app = new Elysia().use(cors()).use(swagger()).use(api_router);
