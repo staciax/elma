6fb351775d01e59c4b3d053c77ee15c45b1e7a84
STACiA <69411527+staciax@users.noreply.github.com>
2024-08-19 20:45:16 +0700

    update env schema

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,27 +1,80 @@
 import z from 'zod';
 
-const envSchema = z.object({
-	API_V1_STR: z.string().default('/api/v1'),
-	DATABASE_URL: z
-		.string({
-			message: 'DATABASE_URL is required',
-			required_error: 'DATABASE_URL is required',
-		})
-		.url(),
-	NODE_ENV: z
-		.enum(['development', 'production', 'test'])
-		.default('development'),
-	BACKEND_CORS_ORIGINS: z
-		.string()
-		.transform((value) => value.split(','))
-		.pipe(z.string().trim().url().array()),
-	HOSTNAME: z.string().default('localhost'),
-	PORT: z.number({ coerce: true }).positive().max(65535).default(8000),
-	SECRET_KEY: z.string().default(new Bun.CryptoHasher('sha256').digest('hex')),
-	ACCESS_TOKEN_EXPIRE_MINUTES: z.string().default('1d'),
-	FIRST_SUPERUSER: z.string().email(),
-	FIRST_SUPERUSER_PASSWORD: z.string(),
-});
+const envSchema = z
+	.object({
+		API_V1_STR: z
+			.string({
+				description: 'API version',
+			})
+			.trim()
+			.startsWith('/', { message: "A path prefix must start with '/'" })
+			.refine((value) => !value.endsWith('/'), {
+				message:
+					"A path prefix must not end with '/', as the routes will start with '/'",
+			})
+			.default('/api/v1'),
+		DATABASE_URL: z
+			.string({
+				message: 'DATABASE_URL is required',
+				description: 'Database connection URL',
+			})
+			.url(),
+		NODE_ENV: z
+			.enum(['development', 'production', 'test'], {
+				message: 'NODE_ENV must be one of "development", "production", "test"',
+				description: 'Node environment',
+			})
+			.default('development'),
+		BACKEND_CORS_ORIGINS: z
+			.string({
+				description: 'Comma-separated list of origins for the CORS policy',
+			})
+			.optional()
+			.transform((value) => (value ? value.split(',') : undefined))
+			.pipe(
+				z
+					.union([z.string().trim().url(), z.literal('*')])
+					.array()
+					.default([]),
+			),
+		HOSTNAME: z
+			.string({
+				description: 'API hostname',
+			})
+			.default('localhost'), // TODO: wait for support hostname https://github.com/colinhacks/zod/pull/3692
+		PORT: z
+			.number({
+				coerce: true,
+				message: 'PORT must be a number',
+				description: 'API port',
+			})
+			.positive({ message: 'PORT must be a positive number' })
+			.min(1000, { message: 'PORT should be >= 1000 and < 65536' })
+			.max(65535, { message: 'PORT should be >= 1000 and < 65536' })
+			.default(8000),
+		SECRET_KEY: z
+			.string({
+				description: 'Secret key for hashing',
+			})
+			.default(new Bun.CryptoHasher('sha256').digest('hex')),
+		ACCESS_TOKEN_EXPIRE: z
+			.string({
+				description: 'Access token expiration time',
+			})
+			.default('1d')
+			.refine((value) => /^\d+[smhdwMy]$/.test(value)),
+		FIRST_SUPERUSER: z
+			.string({
+				message: 'FIRST_SUPERUSER is required',
+				description: 'First superuser email',
+			})
+			.email(),
+		FIRST_SUPERUSER_PASSWORD: z.string({
+			message: 'FIRST_SUPERUSER_PASSWORD is required',
+			description: 'First superuser password',
+		}),
+	})
+	.readonly();
 
 const envServer = envSchema.safeParse(Bun.env);
 
