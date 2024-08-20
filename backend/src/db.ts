714092aa4026ebc4bd3318df7ff3a09de7f54ab5
STACiA <69411527+staciax@users.noreply.github.com>
2024-08-20 19:27:27 +0700

    improvement env schema

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,10 +1,4 @@
-// import { env } from '@/config';
+import { env } from '@/config';
 import mysql from 'mysql2/promise';
 
-export const pool = mysql.createPool({
-	host: 'localhost',
-	user: 'root',
-	database: 'elma',
-	port: 3306,
-	password: 'testuser',
-});
+export const pool = mysql.createPool(env.DATABASE_URI);
