614f80d506e73984f652e0ad3fdf5559905118cb
STACiA <69411527+staciax@users.noreply.github.com>
2024-08-20 13:03:57 +0700

    remove prisma

--- backend/src/api/routes/products.ts
+++ backend/src/api/routes/products.ts
@@ -1,8 +1,8 @@
 import { HTTPError } from '@/errors';
-import { prisma } from '@/lib/prisma';
+// import { prisma } from '@/lib/prisma';
 import { superuser } from '@/plugins/auth';
 
-import { Prisma } from '@prisma/client';
+// import { Prisma } from '@prisma/client';
 import { Elysia, t } from 'elysia';
 import { v7 as uuidv7 } from 'uuid';
 
@@ -13,325 +13,324 @@ import { v7 as uuidv7 } from 'uuid';
 export const router = new Elysia({
 	prefix: '/products',
 	tags: ['products'],
-})
-	.use(superuser())
-	.get(
-		'/',
-		async ({ query: { limit, offset } }) => {
-			const count_stmt = Prisma.raw(
-				'SELECT COUNT(*)::INTEGER as count FROM products',
-			);
-			const count =
-				await prisma.$queryRaw<Record<'count', bigint>[]>(count_stmt);
+}).use(superuser());
+// .get(
+// 	'/',
+// 	async ({ query: { limit, offset } }) => {
+// 		const count_stmt = Prisma.raw(
+// 			'SELECT COUNT(*)::INTEGER as count FROM products',
+// 		);
+// 		const count =
+// 			await prisma.$queryRaw<Record<'count', bigint>[]>(count_stmt);
 
-			const product_stmt = Prisma.raw(`
-			SELECT
-				p.product_id AS product_id,
-				p.title AS product_title,
-				p.description AS product_description,
-				p.isbn AS product_isbn,
-				p.price AS product_price,
-				p.published_date AS product_published_date,
+// 		const product_stmt = Prisma.raw(`
+// 		SELECT
+// 			p.product_id AS product_id,
+// 			p.title AS product_title,
+// 			p.description AS product_description,
+// 			p.isbn AS product_isbn,
+// 			p.price AS product_price,
+// 			p.published_date AS product_published_date,
 
-				pub.publisher_id AS publisher_id,
-				pub.name AS publisher_name,
+// 			pub.publisher_id AS publisher_id,
+// 			pub.name AS publisher_name,
 
-				cat.category_id AS category_id,
-				cat.name AS category_name,
+// 			cat.category_id AS category_id,
+// 			cat.name AS category_name,
 
-				ser.series_id AS series_id,
-				ser.name AS series_name,
+// 			ser.series_id AS series_id,
+// 			ser.name AS series_name,
 
-				a.author_id AS author_id,
-				a.first_name AS author_first_name,
-				a.last_name AS author_last_name,
+// 			a.author_id AS author_id,
+// 			a.first_name AS author_first_name,
+// 			a.last_name AS author_last_name,
 
-				pi.id AS product_image_id,
-				pi.url AS product_image_url
-			FROM
-				products AS p
-			LEFT JOIN
-				publishers AS pub ON p.publisher_id = pub.publisher_id
-			LEFT JOIN
-				categories AS cat ON p.category_id = cat.category_id
-			LEFT JOIN
-				series AS ser ON p.series_id = ser.series_id
-			LEFT JOIN
-				product_authors AS pa ON p.product_id = pa.product_id
-			LEFT JOIN
-				authors AS a ON pa.author_id = a.author_id
-			LEFT JOIN
-				product_images AS pi ON p.product_id = pi.product_id
-			LIMIT ${limit} OFFSET ${offset}
-		`);
-			// GROUP BY
-			//	p.product_id, pub.publisher_id, cat.category_id, ser.series_id, a.author_id, pi.id
+// 			pi.id AS product_image_id,
+// 			pi.url AS product_image_url
+// 		FROM
+// 			products AS p
+// 		LEFT JOIN
+// 			publishers AS pub ON p.publisher_id = pub.publisher_id
+// 		LEFT JOIN
+// 			categories AS cat ON p.category_id = cat.category_id
+// 		LEFT JOIN
+// 			series AS ser ON p.series_id = ser.series_id
+// 		LEFT JOIN
+// 			product_authors AS pa ON p.product_id = pa.product_id
+// 		LEFT JOIN
+// 			authors AS a ON pa.author_id = a.author_id
+// 		LEFT JOIN
+// 			product_images AS pi ON p.product_id = pi.product_id
+// 		LIMIT ${limit} OFFSET ${offset}
+// 	`);
+// 		// GROUP BY
+// 		//	p.product_id, pub.publisher_id, cat.category_id, ser.series_id, a.author_id, pi.id
 
-			const products = await prisma.$queryRaw(product_stmt);
+// 		const products = await prisma.$queryRaw(product_stmt);
 
-			const result = [
-				{
-					count: count[0].count,
-					data: products,
-				},
-			];
+// 		const result = [
+// 			{
+// 				count: count[0].count,
+// 				data: products,
+// 			},
+// 		];
 
-			return result;
-		},
-		{
-			query: t.Object({
-				limit: t.Optional(t.Number({ default: 100 })),
-				offset: t.Optional(t.Number({ default: 0 })),
-			}),
-		},
-	)
-	.get(
-		'/:id',
-		async ({ params: { id } }) => {
-			const stmt = Prisma.raw(`
-				SELECT
-					p.product_id AS product_id,
-					p.title AS product_title,
-					p.description AS product_description,
-					p.isbn AS product_isbn,
-					p.price AS product_price,
-					p.published_date AS product_published_date,
-	
-					pub.publisher_id AS publisher_id,
-					pub.name AS publisher_name,
-	
-					cat.category_id AS category_id,
-					cat.name AS category_name,
-	
-					ser.series_id AS series_id,
-					ser.name AS series_name,
-	
-					a.author_id AS author_id,
-					a.first_name AS author_first_name,
-					a.last_name AS author_last_name,
-	
-					pi.id AS product_image_id,
-					pi.url AS product_image_url
-				FROM
-					products AS p
-				LEFT JOIN
-					publishers AS pub ON p.publisher_id = pub.publisher_id
-				LEFT JOIN
-					categories AS cat ON p.category_id = cat.category_id
-				LEFT JOIN
-					series AS ser ON p.series_id = ser.series_id
-				LEFT JOIN
-					product_authors AS pa ON p.product_id = pa.product_id
-				LEFT JOIN
-					authors AS a ON pa.author_id = a.author_id
-				LEFT JOIN
-					product_images AS pi ON p.product_id = pi.product_id
-				WHERE p.product_id = '${id}'
-			`);
-			// TODO: remove duplicate code
+// 		return result;
+// 	},
+// 	{
+// 		query: t.Object({
+// 			limit: t.Optional(t.Number({ default: 100 })),
+// 			offset: t.Optional(t.Number({ default: 0 })),
+// 		}),
+// 	},
+// )
+// .get(
+// 	'/:id',
+// 	async ({ params: { id } }) => {
+// 		const stmt = Prisma.raw(`
+// 			SELECT
+// 				p.product_id AS product_id,
+// 				p.title AS product_title,
+// 				p.description AS product_description,
+// 				p.isbn AS product_isbn,
+// 				p.price AS product_price,
+// 				p.published_date AS product_published_date,
 
-			// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-			const result = await prisma.$queryRaw<any[]>(stmt);
+// 				pub.publisher_id AS publisher_id,
+// 				pub.name AS publisher_name,
 
-			if (!result.length) {
-				throw new HTTPError(404, 'Product not found');
-			}
+// 				cat.category_id AS category_id,
+// 				cat.name AS category_name,
 
-			return result;
-		},
-		{
-			params: t.Object({
-				id: t.String(),
-			}),
-		},
-	)
-	.post(
-		'/',
-		async ({ body: { title, description, isbn, price, published_date } }) => {
-			// TODO: remove hard-coded values
-			// TODO: make function for get publisher, category, series, authors
+// 				ser.series_id AS series_id,
+// 				ser.name AS series_name,
 
-			// publisher
-			const publisher_id = '019141d2-e151-7033-ac9e-dfb4e8975224'; //body.publisher_id;
-			if (!publisher_id) {
-				throw new HTTPError(400, 'publisher');
-			}
-			// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-			const publisher = await prisma.$queryRaw<any[]>`
-			SELECT
-				*
-			FROM
-				publishers as pub
-			WHERE
-				pub.publisher_id = ${publisher_id}`;
-			if (publisher_id && !publisher.length) {
-				throw new HTTPError(404, 'publisher');
-			}
+// 				a.author_id AS author_id,
+// 				a.first_name AS author_first_name,
+// 				a.last_name AS author_last_name,
 
-			// category
-			const category_id = '019141d2-e153-7a13-9ce5-50aead8148f4'; //body.category_id;
-			if (!category_id) {
-				throw new HTTPError(400, 'category');
-			}
-			// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-			const category = await prisma.$queryRaw<any[]>`
-			SELECT
-				*
-			FROM
-				categories as cat
-			WHERE
-				cat.category_id = ${category_id}`;
-			if (category_id && !category.length) {
-				throw new HTTPError(404, 'category');
-			}
+// 				pi.id AS product_image_id,
+// 				pi.url AS product_image_url
+// 			FROM
+// 				products AS p
+// 			LEFT JOIN
+// 				publishers AS pub ON p.publisher_id = pub.publisher_id
+// 			LEFT JOIN
+// 				categories AS cat ON p.category_id = cat.category_id
+// 			LEFT JOIN
+// 				series AS ser ON p.series_id = ser.series_id
+// 			LEFT JOIN
+// 				product_authors AS pa ON p.product_id = pa.product_id
+// 			LEFT JOIN
+// 				authors AS a ON pa.author_id = a.author_id
+// 			LEFT JOIN
+// 				product_images AS pi ON p.product_id = pi.product_id
+// 			WHERE p.product_id = '${id}'
+// 		`);
+// 		// TODO: remove duplicate code
 
-			// series
-			const series_id = '019141d8-29a4-70d2-b845-2b26ef3a0f5c'; //body.series_id;
-			if (!series_id) {
-				throw new HTTPError(400, 'series');
-			}
-			// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-			const series = await prisma.$queryRaw<any[]>`
-			SELECT
-				*
-			FROM
-				series as ser
-			WHERE
-				ser.series_id = ${series_id}`;
-			if (series_id && !series.length) {
-				throw new HTTPError(404, 'category');
-			}
+// 		// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+// 		const result = await prisma.$queryRaw<any[]>(stmt);
 
-			// TODO: remove author add and make route for add author
-			// authors
-			const author_ids = [
-				'019141d8-29a4-70d2-b845-2b3edf7c59b3',
-				'019141d8-29a5-78c1-a602-3f4535f973b2',
-				'019141d8-3af7-78f0-ba8b-a610a7bd2a3c',
-			];
-			// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-			const authors = await prisma.$queryRaw<any[]>`
-			SELECT
-				*
-			FROM
-				authors AS a
-			WHERE
-				a.author_id IN (${Prisma.join(author_ids)})`;
+// 		if (!result.length) {
+// 			throw new HTTPError(404, 'Product not found');
+// 		}
 
-			if (author_ids.length !== authors.length) {
-				const missing_authors = author_ids.filter(
-					(id) => !authors.find((a) => a.author_id === id),
-				);
-				throw new HTTPError(404, `Author(s) not found: ${missing_authors}`);
-			}
+// 		return result;
+// 	},
+// 	{
+// 		params: t.Object({
+// 			id: t.String(),
+// 		}),
+// 	},
+// )
+// .post(
+// 	'/',
+// 	async ({ body: { title, description, isbn, price, published_date } }) => {
+// 		// TODO: remove hard-coded values
+// 		// TODO: make function for get publisher, category, series, authors
 
-			// TODO: subquery insert product, insert product_authors?
-			// https://stackoverflow.com/questions/56870713/returning-in-insert-into-statement-with-a-select-statement
-			const product_stmt = Prisma.raw(`
-				INSERT INTO products as p (
-					product_id,
-					title,
-					description,
-					isbn,
-					price,
-					published_date,
-					publisher_id,
-					category_id,
-					series_id
-				)
-				VALUES (
-				 	'${uuidv7()}',
-					'${title}',
-					'${description}', 
-					'${isbn}',
-					${new Prisma.Decimal(price)},
-					'${new Date(published_date).toISOString()}',
-					'${publisher_id}',
-					'${category_id}',
-					'${series_id}'
-				)
-				RETURNING
-					p.product_id AS product_id,
-					p.title AS product_title,
-					p.description AS product_description,
-					p.isbn AS product_isbn,
-					p.price AS product_price,
-					p.published_date AS product_published_date,
-					p.publisher_id AS publisher_id,
-					p.category_id AS category_id,
-					p.series_id AS series_id
+// 		// publisher
+// 		const publisher_id = '019141d2-e151-7033-ac9e-dfb4e8975224'; //body.publisher_id;
+// 		if (!publisher_id) {
+// 			throw new HTTPError(400, 'publisher');
+// 		}
+// 		// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+// 		const publisher = await prisma.$queryRaw<any[]>`
+// 		SELECT
+// 			*
+// 		FROM
+// 			publishers as pub
+// 		WHERE
+// 			pub.publisher_id = ${publisher_id}`;
+// 		if (publisher_id && !publisher.length) {
+// 			throw new HTTPError(404, 'publisher');
+// 		}
 
-			`);
-			const result = await prisma.$queryRaw(product_stmt);
+// 		// category
+// 		const category_id = '019141d2-e153-7a13-9ce5-50aead8148f4'; //body.category_id;
+// 		if (!category_id) {
+// 			throw new HTTPError(400, 'category');
+// 		}
+// 		// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+// 		const category = await prisma.$queryRaw<any[]>`
+// 		SELECT
+// 			*
+// 		FROM
+// 			categories as cat
+// 		WHERE
+// 			cat.category_id = ${category_id}`;
+// 		if (category_id && !category.length) {
+// 			throw new HTTPError(404, 'category');
+// 		}
 
-			return result;
-		},
-		{
-			body: t.Object({
-				title: t.String({ minLength: 1, maxLength: 255 }),
-				description: t.String({ minLength: 1 }),
-				isbn: t.String({ maxLength: 13 }),
-				price: t.Number({ minimum: 0 }), // TODO: maximum ???
-				published_date: t.Date(),
-				publisher_id: t.Optional(t.String()),
-				category_id: t.Optional(t.String()),
-				series_id: t.Optional(t.String()),
-				author_ids: t.Optional(t.Array(t.String())),
-			}),
-		},
-	)
-	.patch('/:id', async ({ params: { id } }) => {
-		const product_stmt = Prisma.raw(`
-		SELECT
-			p.product_id AS product_id
-		FROM
-			products AS p
-		WHERE
-			p.product_id = '${id}'`);
+// 		// series
+// 		const series_id = '019141d8-29a4-70d2-b845-2b26ef3a0f5c'; //body.series_id;
+// 		if (!series_id) {
+// 			throw new HTTPError(400, 'series');
+// 		}
+// 		// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+// 		const series = await prisma.$queryRaw<any[]>`
+// 		SELECT
+// 			*
+// 		FROM
+// 			series as ser
+// 		WHERE
+// 			ser.series_id = ${series_id}`;
+// 		if (series_id && !series.length) {
+// 			throw new HTTPError(404, 'category');
+// 		}
 
-		// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-		const product = await prisma.$queryRaw<any[]>(product_stmt);
+// 		// TODO: remove author add and make route for add author
+// 		// authors
+// 		const author_ids = [
+// 			'019141d8-29a4-70d2-b845-2b3edf7c59b3',
+// 			'019141d8-29a5-78c1-a602-3f4535f973b2',
+// 			'019141d8-3af7-78f0-ba8b-a610a7bd2a3c',
+// 		];
+// 		// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+// 		const authors = await prisma.$queryRaw<any[]>`
+// 		SELECT
+// 			*
+// 		FROM
+// 			authors AS a
+// 		WHERE
+// 			a.author_id IN (${Prisma.join(author_ids)})`;
 
-		if (!product.length) {
-			throw new HTTPError(404, 'Product not found');
-		}
-	})
-	.delete(
-		'/:id',
-		async ({ params: { id } }) => {
-			const product_stmt = Prisma.raw(`
-			SELECT
-				p.product_id AS product_id
-			FROM
-				products AS p
-			WHERE
-				p.product_id = '${id}'`);
+// 		if (author_ids.length !== authors.length) {
+// 			const missing_authors = author_ids.filter(
+// 				(id) => !authors.find((a) => a.author_id === id),
+// 			);
+// 			throw new HTTPError(404, `Author(s) not found: ${missing_authors}`);
+// 		}
 
-			// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-			const product = await prisma.$queryRaw<any[]>(product_stmt);
+// 		// TODO: subquery insert product, insert product_authors?
+// 		// https://stackoverflow.com/questions/56870713/returning-in-insert-into-statement-with-a-select-statement
+// 		const product_stmt = Prisma.raw(`
+// 			INSERT INTO products as p (
+// 				product_id,
+// 				title,
+// 				description,
+// 				isbn,
+// 				price,
+// 				published_date,
+// 				publisher_id,
+// 				category_id,
+// 				series_id
+// 			)
+// 			VALUES (
+// 			 	'${uuidv7()}',
+// 				'${title}',
+// 				'${description}',
+// 				'${isbn}',
+// 				${new Prisma.Decimal(price)},
+// 				'${new Date(published_date).toISOString()}',
+// 				'${publisher_id}',
+// 				'${category_id}',
+// 				'${series_id}'
+// 			)
+// 			RETURNING
+// 				p.product_id AS product_id,
+// 				p.title AS product_title,
+// 				p.description AS product_description,
+// 				p.isbn AS product_isbn,
+// 				p.price AS product_price,
+// 				p.published_date AS product_published_date,
+// 				p.publisher_id AS publisher_id,
+// 				p.category_id AS category_id,
+// 				p.series_id AS series_id
 
-			if (!product.length) {
-				throw new HTTPError(404, 'Product not found');
-			}
+// 		`);
+// 		const result = await prisma.$queryRaw(product_stmt);
 
-			const delete_stmt = Prisma.raw(`
-			DELETE
-			FROM
-				products AS p
-			WHERE
-				p.product_id = '${id}'`);
-			const deleted = await prisma.$executeRaw(delete_stmt);
-			if (!deleted) {
-				throw new HTTPError(500, 'Product not deleted');
-			}
+// 		return result;
+// 	},
+// 	{
+// 		body: t.Object({
+// 			title: t.String({ minLength: 1, maxLength: 255 }),
+// 			description: t.String({ minLength: 1 }),
+// 			isbn: t.String({ maxLength: 13 }),
+// 			price: t.Number({ minimum: 0 }), // TODO: maximum ???
+// 			published_date: t.Date(),
+// 			publisher_id: t.Optional(t.String()),
+// 			category_id: t.Optional(t.String()),
+// 			series_id: t.Optional(t.String()),
+// 			author_ids: t.Optional(t.Array(t.String())),
+// 		}),
+// 	},
+// )
+// .patch('/:id', async ({ params: { id } }) => {
+// 	const product_stmt = Prisma.raw(`
+// 	SELECT
+// 		p.product_id AS product_id
+// 	FROM
+// 		products AS p
+// 	WHERE
+// 		p.product_id = '${id}'`);
 
-			return {
-				message: 'Product deleted successfully',
-			};
-		},
-		{
-			params: t.Object({
-				id: t.String(),
-			}),
-		},
-	);
+// 	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+// 	const product = await prisma.$queryRaw<any[]>(product_stmt);
+
+// 	if (!product.length) {
+// 		throw new HTTPError(404, 'Product not found');
+// 	}
+// })
+// .delete(
+// 	'/:id',
+// 	async ({ params: { id } }) => {
+// 		const product_stmt = Prisma.raw(`
+// 		SELECT
+// 			p.product_id AS product_id
+// 		FROM
+// 			products AS p
+// 		WHERE
+// 			p.product_id = '${id}'`);
+
+// 		// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+// 		const product = await prisma.$queryRaw<any[]>(product_stmt);
+
+// 		if (!product.length) {
+// 			throw new HTTPError(404, 'Product not found');
+// 		}
+
+// 		const delete_stmt = Prisma.raw(`
+// 		DELETE
+// 		FROM
+// 			products AS p
+// 		WHERE
+// 			p.product_id = '${id}'`);
+// 		const deleted = await prisma.$executeRaw(delete_stmt);
+// 		if (!deleted) {
+// 			throw new HTTPError(500, 'Product not deleted');
+// 		}
+
+// 		return {
+// 			message: 'Product deleted successfully',
+// 		};
+// 	},
+// 	{
+// 		params: t.Object({
+// 			id: t.String(),
+// 		}),
+// 	},
+// );
