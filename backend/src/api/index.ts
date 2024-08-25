b0dc8d1bb28f80f9d06607c01a482d88adf414e0
STACiA <69411527+staciax@users.noreply.github.com>
2024-08-25 20:55:31 +0700

    renamed import

--- backend/src/api/index.ts
+++ backend/src/api/index.ts
@@ -2,20 +2,20 @@ import { env } from '@/config';
 
 import { Elysia } from 'elysia';
 import { router as authRouter } from './routes/auth';
-import { router as authorRouter } from './routes/authors';
-import { router as categoryRouter } from './routes/categories';
-import { router as orderRouter } from './routes/orders';
-import { router as productRouter } from './routes/products';
-import { router as publisherRouter } from './routes/publishers';
-import { router as shoppingCartRouter } from './routes/shopping-carts';
-import { router as userRouter } from './routes/users';
+import { router as authorsRouter } from './routes/authors';
+import { router as categoriesRouter } from './routes/categories';
+import { router as ordersRouter } from './routes/orders';
+import { router as productsRouter } from './routes/products';
+import { router as publishersRouter } from './routes/publishers';
+import { router as shoppingCartsRouter } from './routes/shopping-carts';
+import { router as usersRouter } from './routes/users';
 
 export const router = new Elysia({ prefix: env.API_V1_STR }) //
 	.use(authRouter)
-	.use(userRouter)
-	.use(authorRouter)
-	.use(publisherRouter)
-	.use(categoryRouter)
-	.use(productRouter)
-	.use(shoppingCartRouter)
-	.use(orderRouter);
+	.use(usersRouter)
+	.use(authorsRouter)
+	.use(publishersRouter)
+	.use(categoriesRouter)
+	.use(productsRouter)
+	.use(shoppingCartsRouter)
+	.use(ordersRouter);
