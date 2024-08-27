a8d1d2ec3cb3d01de640ff0bdec081d491a77cd3
STACiA <69411527+staciax@users.noreply.github.com>
2024-08-27 19:11:21 +0700

    backup testfile

---
+++ backend/test_array.ts
@@ -0,0 +1,13 @@
+const x = [
+	{
+		name: 'A',
+	},
+	{
+		name: 'B',
+	},
+];
+const reverse = true;
+
+const x2 = reverse ? x.reverse() : x;
+
+console.log(x2);
---
+++ backend/test_z2.ts
@@ -0,0 +1,35 @@
+import z from 'zod';
+
+import { env } from '@/config';
+import jwt from '@elysiajs/jwt';
+
+const schema = z
+	.object({
+		db_host: z.string(),
+		db_port: z.number(),
+		db_name: z.string(),
+		db_user: z.string(),
+		db_password: z.string(),
+	})
+	.transform((values) => ({
+		...values,
+		get db_url(): string {
+			return `mysql://${values.db_user}:${values.db_password}@${values.db_host}:${values.db_port}/${values.db_name}`;
+		},
+	}));
+
+const result = schema.safeParse({
+	db_host: 'localhost',
+	db_port: 3306,
+	db_name: 'classicmodels',
+	db_user: 'root',
+	db_password: 'testuser',
+});
+
+z.string().url().parse('foo');
+
+console.log(result.data?.db_host);
+
+if (result.data) {
+	console.log('foo is not empty');
+}
