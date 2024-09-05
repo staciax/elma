836273a53c201483d50416ee63c28847e6298aa6
STACiA <69411527+staciax@users.noreply.github.com>
2024-09-06 02:28:33 +0700

    implement header

--- frontend/components/header.tsx
+++ frontend/components/header.tsx
@@ -1,15 +1,31 @@
+import { Input } from '@/components/ui/input';
+import { Search } from 'lucide-react';
+import Link from 'next/link';
+
 function Header() {
 	return (
-		<header>
-			<div className="container mx-auto flex flex-row items-center justify-between ">
-				<div>
-					<span className="text-2xl">STACIA.</span>
-				</div>
-				<div>
-					<input type="text" className="indent-2" />
-				</div>
-				<div>auth</div>
+		<header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+			<div>
+				<Link href="/">
+					<span className="font-semibold text-xl">ELMA</span>
+				</Link>
 			</div>
+			<div className="flex w-full items-center gap-4 md:mx-auto md:gap-2 lg:gap-4">
+				<form className="mx-auto flex-1 sm:flex-initial">
+					<div className="relative">
+						<button type="submit" className="absolute top-2.5 right-2.5">
+							<Search className="h-4 w-4 text-muted-foreground" />
+						</button>
+						<Input
+							name="q"
+							type="text"
+							placeholder="ค้นหาสินค้า..."
+							className="pl-4 sm:w-[300px] md:w-[200px] lg:w-[300px]"
+						/>
+					</div>
+				</form>
+			</div>
+			<nav>NAV</nav>
 		</header>
 	);
 }
