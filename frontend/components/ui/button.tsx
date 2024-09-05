ecc4be123ff57fc600e5187be3739af5a4870a3e
STACiA <69411527+staciax@users.noreply.github.com>
2024-09-06 02:29:24 +0700

    fmt

--- frontend/components/ui/button.tsx
+++ frontend/components/ui/button.tsx
@@ -1,5 +1,4 @@
 import { cn } from '@/lib/utils';
-
 import { Slot } from '@radix-ui/react-slot';
 import { type VariantProps, cva } from 'class-variance-authority';
 import * as React from 'react';
