cb759458655e5b15d4a5491d53ac198080512dbc
STACiA <69411527+staciax@users.noreply.github.com>
2024-09-06 17:38:59 +0700

    fetch products

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,18 @@
-export default function Home() {
+import { getProducts } from '@/lib/elma/actions/products';
+import Link from 'next/link';
+
+export default async function Home() {
+	const results = await getProducts();
+	const products = results.data;
 	return (
 		<main>
-			<div>test</div>
+			{products.map((product) => (
+				<div key={product.product_id}>
+					<Link href={`/product/${product.product_id}`}>
+						<h2>{product.product_title}</h2>
+					</Link>
+				</div>
+			))}
 		</main>
 	);
 }
