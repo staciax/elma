cbc76965532f65850f288451a7f6c579c1747c5b
STACiA <69411527+staciax@users.noreply.github.com>
2024-09-07 01:09:38 +0700

    implement footer

--- frontend/app/(routes)/(home)/layout.tsx
+++ frontend/app/(routes)/(home)/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from '@/components/footer';
 import Header from '@/components/header';
 
 export default function HomeLayout({
@@ -9,6 +10,7 @@ export default function HomeLayout({
 		<>
 			<Header />
 			{children}
+			<Footer />
 		</>
 	);
 }
--- frontend/components/footer.tsx
+++ frontend/components/footer.tsx
@@ -1,5 +1,24 @@
+import Link from 'next/link';
+
 function Footer() {
-	return <div>Footer</div>;
+	return (
+		<footer className="bg-background">
+			<section className="flex h-10 items-center justify-center space-x-6">
+				<span className="text-[#737373] text-sm">&copy; 2024 Latte Book Store., Ltd. All rights reserved</span>
+				<div className="space-x-4">
+					<Link href="#" className="select-none text-xs">
+						ข้อกำหนดและเงื่อนไขการใช้งาน
+					</Link>
+					<Link href="#" className="select-none text-xs">
+						นโยบายความเป็นส่วนตัว
+					</Link>
+					<Link href="#" className="select-none text-xs">
+						นโยบายการใช้งานคุกกี้
+					</Link>
+				</div>
+			</section>
+		</footer>
+	);
 }
 
 export default Footer;
