a5f53c0b5d839c88eb239fee578a296f2e821886
STACiA <69411527+staciax@users.noreply.github.com>
2024-09-06 17:36:44 +0700

    add product type, fix token type, add fetch products

--- frontend/lib/elma/actions/products.ts
+++ frontend/lib/elma/actions/products.ts
@@ -1,25 +1,21 @@
 'use server';
 import axios from '@/lib/axios';
 
-type Product = {
-	product_id: string;
-	product_title: string;
-	product_description: string;
-	product_isbn: string;
-	product_ebook_price: number;
-	product_paper_price: number;
-	// product_ebook_file: string;
-	published_date: string;
-	publisher_id?: string;
-	publisher_name?: string;
-	category_id?: string;
-	category_name?: string;
-};
+import type { Product, Products } from '../types/products';
 
 export const getProduct = async (id: string): Promise<Product[]> => {
 	console.log('product id:', id);
 	try {
 		const response = await axios.get(`/v1/products/${id}/`);
+		return response.data;
+	} catch (error) {
+		return Promise.reject(error);
+	}
+};
+
+export const getProducts = async (): Promise<Products> => {
+	try {
+		const response = await axios.get('/v1/products/');
 		return response.data;
 	} catch (error) {
 		return Promise.reject(error);
