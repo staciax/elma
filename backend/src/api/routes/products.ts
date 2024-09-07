76fc4dbae42c3b23026a47a23f7099e73758efbc
STACiA <69411527+staciax@users.noreply.github.com>
2024-09-07 17:38:58 +0700

    fix product can't get without auth

--- backend/src/api/routes/products.ts
+++ backend/src/api/routes/products.ts
@@ -16,7 +16,7 @@ export const router = new Elysia({
 	tags: ['products'],
 })
 	// TODO: add role-based access control
-	.use(superuser())
+
 	.get(
 		'/',
 		async ({ query: { limit, offset } }) => {
@@ -30,28 +30,28 @@ export const router = new Elysia({
 			const count = count_results[0].count;
 
 			const product_stmt = `
-				SELECT
-					product.id AS product_id,
-					product.title AS product_title,
-					product.description AS product_description,
-					product.isbn AS product_isbn,
-					-- product.price AS product_price,
-					product.price AS product_ebook_price,
-					product.price AS product_paper_price,
-					product.published_date AS product_published_date,
+			SELECT
+				product.id AS product_id,
+				product.title AS product_title,
+				product.description AS product_description,
+				product.isbn AS product_isbn,
+				-- product.price AS product_price,
+				product.price AS product_ebook_price,
+				product.price AS product_paper_price,
+				product.published_date AS product_published_date,
 
-					publisher.id AS publisher_id,
-					publisher.name AS publisher_name,
+				publisher.id AS publisher_id,
+				publisher.name AS publisher_name,
 
-					category.id AS category_id,
-					category.name AS category_name
-				FROM
-					products AS product
-				LEFT JOIN
-			 		publishers AS publisher ON product.publisher_id = publisher.id
-				LEFT JOIN
-					categories AS category ON product.category_id = category.id
-				LIMIT ? OFFSET ?
+				category.id AS category_id,
+				category.name AS category_name
+			FROM
+				products AS product
+			LEFT JOIN
+				publishers AS publisher ON product.publisher_id = publisher.id
+			LEFT JOIN
+				categories AS category ON product.category_id = category.id
+			LIMIT ? OFFSET ?
 			`;
 			// TODO: join product_authors, authors, product_images
 			// and then group by?
@@ -115,196 +115,207 @@ export const router = new Elysia({
 			}),
 		},
 	)
-	.post(
-		'/',
-		async ({
-			set,
-			body: {
-				title,
-				description,
-				isbn,
-				price,
-				published_date,
-				publisher_id,
-				category_id,
-			},
-		}) => {
-			// TODO: remove hard-coded values
-			// TODO: make function for get publisher, category, series, authors
+	.guard((app) =>
+		app
+			.use(superuser())
+			.post(
+				'/',
+				async ({
+					set,
+					body: {
+						title,
+						description,
+						isbn,
+						price,
+						published_date,
+						publisher_id,
+						category_id,
+					},
+				}) => {
+					// TODO: remove hard-coded values
+					// TODO: make function for get publisher, category, series, authors
 
-			const conn = await pool.getConnection();
+					const conn = await pool.getConnection();
 
-			// category
-			// const publisher_id = '0191896f-047a-7332-8f48-5ba16d59404f'; //body.publisher_id;
-			// if (!publisher_id) {
-			// 	throw new HTTPError(400, 'publisher');
-			// }
-			const publisher_stmt = `
-			SELECT
-				*
-			FROM
-			 	publishers
-			WHERE
-			 	id = ?`;
-			const [publisher] = await conn.query<RowDataPacket[]>(publisher_stmt, [
-				publisher_id,
-			]);
-			if (publisher_id && !publisher.length) {
-				throw new HTTPError(404, 'publisher');
-			}
+					// category
+					// const publisher_id = '0191896f-047a-7332-8f48-5ba16d59404f'; //body.publisher_id;
+					// if (!publisher_id) {
+					// 	throw new HTTPError(400, 'publisher');
+					// }
+					const publisher_stmt = `
+					SELECT
+						*
+					FROM
+						publishers
+					WHERE
+						id = ?`;
+					const [publisher] = await conn.query<RowDataPacket[]>(
+						publisher_stmt,
+						[publisher_id],
+					);
+					if (publisher_id && !publisher.length) {
+						throw new HTTPError(404, 'publisher');
+					}
 
-			// category
-			// const category_id = '02a06798-62e6-11ef-877e-0e4d59d62537'; //body.category_id;
-			// if (!category_id) {
-			// 	throw new HTTPError(400, 'category');
-			// }
-			const category_stmt = `
-			SELECT
-				*
-			FROM
-				categories
-			WHERE
-				id = ?`;
-			const [category] = await conn.query<RowDataPacket[]>(category_stmt, [
-				category_id,
-			]);
-			if (category_id && !category.length) {
-				throw new HTTPError(404, 'category');
-			}
+					// category
+					// const category_id = '02a06798-62e6-11ef-877e-0e4d59d62537'; //body.category_id;
+					// if (!category_id) {
+					// 	throw new HTTPError(400, 'category');
+					// }
+					const category_stmt = `
+					SELECT
+						*
+					FROM
+						categories
+					WHERE
+						id = ?`;
+					const [category] = await conn.query<RowDataPacket[]>(category_stmt, [
+						category_id,
+					]);
+					if (category_id && !category.length) {
+						throw new HTTPError(404, 'category');
+					}
 
-			// TODO: product_authors?
+					// TODO: product_authors?
 
-			const product_stmt = `
-				INSERT INTO products (
-					id,
-					title,
-					description,
-					isbn,
-					price,
-					published_date,
-					publisher_id,
-					category_id
-				)
-				VALUES (
-				 	?,
-					?,
-					?,
-					?,
-					?,
-					?,
-					?,
-					?
-				);
-			`;
+					const product_stmt = `
+					INSERT INTO products (
+						id,
+						title,
+						description,
+						isbn,
+						price,
+						published_date,
+						publisher_id,
+						category_id
+					)
+					VALUES (
+						?,
+						?,
+						?,
+						?,
+						?,
+						?,
+						?,
+						?
+					);
+					`;
 
-			await conn.query<ResultSetHeader>(product_stmt, [
-				uuidv7(),
-				title,
-				description,
-				isbn,
-				price,
-				published_date,
-				publisher_id,
-				category_id,
-			]);
+					await conn.query<ResultSetHeader>(product_stmt, [
+						uuidv7(),
+						title,
+						description,
+						isbn,
+						price,
+						published_date,
+						publisher_id,
+						category_id,
+					]);
 
-			// TODO: refresh product and return product data
-			set.status = 201;
-			return { message: 'Product created successfully' };
-		},
-		{
-			body: t.Object({
-				title: t.String({ minLength: 1, maxLength: 255 }),
-				description: t.String({ minLength: 1, maxLength: 65535 }), // NOTE: mysql maximum length of TEXT is 65,535
-				isbn: t.String({ minLength: 13, maxLength: 13 }), // TODO: isbn how many minl
-				price: t.Number({ minimum: 0 }), // TODO: maximum ???
-				published_date: t.Date({ format: 'date-time' }),
-				publisher_id: t.Optional(t.String({ format: 'uuid' })),
-				category_id: t.Optional(t.String({ format: 'uuid' })),
-				// series_id: t.Optional(t.String()),
-				// author_ids: t.Optional(t.Array(t.String())),
-			}),
-		},
-	)
-	.patch(
-		'/:id',
-		async ({ params: { id } }) => {
-			const conn = await pool.getConnection();
-			const product_stmt = `
-		SELECT
-			*
-		FROM
-			products
-		WHERE
-			id = ?`;
+					// TODO: refresh product and return product data
+					set.status = 201;
+					return { message: 'Product created successfully' };
+				},
+				{
+					body: t.Object({
+						title: t.String({ minLength: 1, maxLength: 255 }),
+						description: t.String({ minLength: 1, maxLength: 65535 }), // NOTE: mysql maximum length of TEXT is 65,535
+						isbn: t.String({ minLength: 13, maxLength: 13 }), // TODO: isbn how many minl
+						price: t.Number({ minimum: 0 }), // TODO: maximum ???
+						published_date: t.Date({ format: 'date-time' }),
+						publisher_id: t.Optional(t.String({ format: 'uuid' })),
+						category_id: t.Optional(t.String({ format: 'uuid' })),
+						// series_id: t.Optional(t.String()),
+						// author_ids: t.Optional(t.Array(t.String())),
+					}),
+				},
+			)
+			.patch(
+				'/:id',
+				async ({ params: { id } }) => {
+					const conn = await pool.getConnection();
+					const product_stmt = `
+					SELECT
+						*
+					FROM
+						products
+					WHERE
+						id = ?`;
 
-			const [product] = await conn.query<RowDataPacket[]>(product_stmt, [id]);
-			if (!product.length) {
-				conn.release();
-				throw new HTTPError(404, 'Product not found');
-			}
+					const [product] = await conn.query<RowDataPacket[]>(product_stmt, [
+						id,
+					]);
+					if (!product.length) {
+						conn.release();
+						throw new HTTPError(404, 'Product not found');
+					}
 
-			// TODO: update product
+					// TODO: update product
 
-			conn.release();
+					conn.release();
 
-			return { message: 'Product updated successfully' };
-		},
-		{
-			params: t.Object({
-				id: t.String({ format: 'uuid' }),
-			}),
-			body: t.Object({
-				title: t.Optional(t.String({ minLength: 1, maxLength: 255 })),
-				description: t.Optional(t.String({ minLength: 1, maxLength: 255 })),
-				isbn: t.Optional(t.String({ minLength: 13, maxLength: 13 })),
-				price: t.Optional(t.Number({ minimum: 0 })),
-				published_date: t.Optional(t.Date({ format: 'date-time' })),
-				publisher_id: t.Optional(t.String({ format: 'uuid' })),
-				category_id: t.Optional(t.String({ format: 'uuid' })),
-			}),
-		},
-	)
-	.delete(
-		'/:id',
-		async ({ params: { id } }) => {
-			const conn = await pool.getConnection();
-			const product_stmt = `
-			SELECT
-				*
-			FROM
-				products
-			WHERE
-				id = ?
-			`;
-			const [product] = await conn.query<RowDataPacket[]>(product_stmt, [id]);
+					return { message: 'Product updated successfully' };
+				},
+				{
+					params: t.Object({
+						id: t.String({ format: 'uuid' }),
+					}),
+					body: t.Object({
+						title: t.Optional(t.String({ minLength: 1, maxLength: 255 })),
+						description: t.Optional(t.String({ minLength: 1, maxLength: 255 })),
+						isbn: t.Optional(t.String({ minLength: 13, maxLength: 13 })),
+						price: t.Optional(t.Number({ minimum: 0 })),
+						published_date: t.Optional(t.Date({ format: 'date-time' })),
+						publisher_id: t.Optional(t.String({ format: 'uuid' })),
+						category_id: t.Optional(t.String({ format: 'uuid' })),
+					}),
+				},
+			)
+			.delete(
+				'/:id',
+				async ({ params: { id } }) => {
+					const conn = await pool.getConnection();
+					const product_stmt = `
+					SELECT
+						*
+					FROM
+						products
+					WHERE
+						id = ?
+					`;
+					const [product] = await conn.query<RowDataPacket[]>(product_stmt, [
+						id,
+					]);
 
-			if (!product.length) {
-				conn.release();
-				throw new HTTPError(404, 'Product not found');
-			}
+					if (!product.length) {
+						conn.release();
+						throw new HTTPError(404, 'Product not found');
+					}
 
-			console.log(product);
+					console.log(product);
 
-			const delete_stmt = `
-			DELETE
-			FROM
-				products
-			WHERE
-				id = ?`;
-			const [deleted] = await conn.execute<ResultSetHeader>(delete_stmt, [id]);
-			conn.release();
+					const delete_stmt = `
+					DELETE
+					FROM
+						products
+					WHERE
+						id = ?`;
+					const [deleted] = await conn.execute<ResultSetHeader>(delete_stmt, [
+						id,
+					]);
+					conn.release();
 
-			if (!deleted) {
-				throw new HTTPError(500, 'Product not deleted');
-			}
-			return {
-				message: 'Product deleted successfully',
-			};
-		},
-		{
-			params: t.Object({
-				id: t.String({ format: 'uuid' }),
-			}),
-		},
+					if (!deleted) {
+						throw new HTTPError(500, 'Product not deleted');
+					}
+					return {
+						message: 'Product deleted successfully',
+					};
+				},
+				{
+					params: t.Object({
+						id: t.String({ format: 'uuid' }),
+					}),
+				},
+			),
 	);
