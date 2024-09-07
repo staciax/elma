e3c0a2bc2b592968429825f4e1de424d5d773a39
STACiA <69411527+staciax@users.noreply.github.com>
2024-09-07 22:28:50 +0700

    refactor register page

--- frontend/app/(routes)/(home)/(auth)/register/page.tsx
+++ frontend/app/(routes)/(home)/(auth)/register/page.tsx
@@ -1,10 +1,6 @@
 import SignUpForm from '@/components/forms/sign-up';
-import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
 import type { Metadata } from 'next';
-import Link from 'next/link';
 
 export const metadata: Metadata = {
 	title: 'สมัครสมาชิก',
@@ -15,49 +11,10 @@ export function RegisterForm() {
 		<Card className="mx-auto max-w-sm">
 			<CardHeader>
 				<CardTitle className="text-xl">สมัครสมาชิก</CardTitle>
-				<CardDescription>Enter your information to create an account</CardDescription>
+				<CardDescription>กรอกข้อมูลเพื่อสร้างบัญชีผู้ใช้ของคุณ</CardDescription>
 			</CardHeader>
 			<CardContent>
-				<div className="grid gap-4">
-					<div className="grid grid-cols-2 gap-4">
-						<div className="grid gap-2">
-							<Label htmlFor="first-name">ชื่อ</Label>
-							<Input id="first-name" placeholder="Max" required />
-						</div>
-						<div className="grid gap-2">
-							<Label htmlFor="last-name">นามสกุล</Label>
-							<Input id="last-name" placeholder="Robinson" required />
-						</div>
-					</div>
-					<div className="grid gap-2">
-						<Label htmlFor="email">อีเมล</Label>
-						<Input id="email" type="email" placeholder="m@example.com" required />
-					</div>
-					<div className="grid gap-2">
-						<Label htmlFor="password">รหััสผ่าน</Label>
-						<Input id="password" type="password" />
-					</div>
-					<div className="grid gap-2">
-						<Label htmlFor="password-comfirm">ยืนยันรหัสผ่าน</Label>
-						<Input
-							id="password-comfirm"
-							type="password"
-							className="focus:border-[#5AB772] focus:outline-[#5AB772] focus:ring-[#5AB772]"
-						/>
-					</div>
-					<Button type="submit" className="w-full transition hover:bg-[#5AB772]">
-						สมัครสมาชิก
-					</Button>
-					{/* <Button variant="outline" className="w-full">
-						Sign up with GitHub
-					</Button> */}
-				</div>
-				<div className="mt-4 text-center text-sm">
-					หากมีบัญชีผู้ใช้แล้ว?{' '}
-					<Link href="/login" className="text-[#5AB772] underline">
-						เข้าสู่ระบบ
-					</Link>
-				</div>
+				<SignUpForm />
 			</CardContent>
 		</Card>
 	);
