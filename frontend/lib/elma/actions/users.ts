db4cc42dfb4727448d02f2ba718d6e83be7216de
STACiA <69411527+staciax@users.noreply.github.com>
2024-09-07 22:29:53 +0700

    add users actions

---
+++ frontend/lib/elma/actions/users.ts
@@ -0,0 +1,19 @@
+'use server';
+
+import axios from '@/lib/axios';
+
+type UserMe = {
+	id: number;
+	email: string;
+	first_name: string;
+	last_name: string;
+};
+
+export const getUserMe = async (): Promise<UserMe> => {
+	try {
+		const response = await axios.get('/v1/users/me');
+		return response.data;
+	} catch (error) {
+		return Promise.reject(error);
+	}
+};
