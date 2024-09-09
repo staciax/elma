241ea049331c06c355713ad6bfc96e4b28089b8f
STACiA <69411527+staciax@users.noreply.github.com>
2024-09-09 22:05:21 +0700

    update init superuser

--- backend/src/initial_data.ts
+++ backend/src/initial_data.ts
@@ -1,10 +1,38 @@
 import { env } from '@/config';
+import { pool } from '@/db';
 import { getPasswordHash } from '@/security';
+import { v7 as uuidv7 } from 'uuid';
 
 async function init() {
+	const conn = await pool.getConnection();
+
 	const email = env.FIRST_SUPERUSER;
 	const password = env.FIRST_SUPERUSER_PASSWORD;
 	const hashed_password = await getPasswordHash(password);
+
+	const stmt = `
+	INSERT INTO users
+	(
+		id,
+		email,
+		hashed_password,
+		role,
+		is_active
+	)
+	VALUES
+	(
+		?,
+		?,
+		?,
+		?,
+		?
+	);
+	`;
+
+	await conn.query(stmt, [uuidv7(), email, hashed_password, 'SUPERUSER', true]);
+
+	conn.release();
+	await pool.end();
 }
 
 async function main() {
