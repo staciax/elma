37bf3d1e03a40ce5cf88ea302e4efedec0187b96
STACiA <69411527+staciax@users.noreply.github.com>
2024-09-09 22:05:45 +0700

    query schema

---
+++ backend/src/schemas/query.ts
@@ -0,0 +1,9 @@
+// import { t } from 'elysia';
+
+// export const LimitOffset = t.Object({
+// 	limit: t.Optional(t.Integer({ minimum: 1, default: 100 })),
+// 	offset: t.Optional(t.Integer({ minimum: 0, default: 0 })),
+// });
+
+// const Limit = t.Optional(t.Integer({ minimum: 1, default: 100 }));
+// const Offset = t.Optional(t.Integer({ minimum: 0, default: 0 }));
