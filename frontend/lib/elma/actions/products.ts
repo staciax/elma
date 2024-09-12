e63bc0a876e4d3ecf3b699c3e2adeff2e1b9492b
STACiA <69411527+staciax@users.noreply.github.com>
2024-09-13 01:04:25 +0700

    fix product type and add options

--- frontend/lib/elma/actions/products.ts
+++ frontend/lib/elma/actions/products.ts
@@ -1,9 +1,9 @@
 'use server';
 import axios from '@/lib/axios';
 
-import type { Product, Products } from '../types/products';
+import type { ProductPublic, ProductsPublic } from '../types/products';
 
-export const getProduct = async (id: string): Promise<Product[]> => {
+export const getProduct = async (id: string): Promise<ProductPublic[]> => {
 	console.log('product id:', id);
 	try {
 		const response = await axios.get(`/v1/products/${id}/`);
@@ -13,9 +13,14 @@ export const getProduct = async (id: string): Promise<Product[]> => {
 	}
 };
 
-export const getProducts = async (): Promise<Products> => {
+export const getProducts = async (options?: {
+	limit?: number;
+	offset?: number;
+}): Promise<ProductsPublic> => {
 	try {
-		const response = await axios.get('/v1/products/');
+		const response = await axios.get('/v1/products/', {
+			params: options,
+		});
 		return response.data;
 	} catch (error) {
 		return Promise.reject(error);
