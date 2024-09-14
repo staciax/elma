1fd3beb9dc5ce93b2e9304b7ce6b3d1c6608b40a
STACiA <69411527+staciax@users.noreply.github.com>
2024-09-15 02:26:48 +0700

    update authors router

--- backend/src/api/routes/authors.ts
+++ backend/src/api/routes/authors.ts
@@ -1,4 +1,5 @@
 import { pool } from '@/db';
+import { HTTPError } from '@/errors';
 import { superuser } from '@/plugins/auth';
 
 import { Elysia, t } from 'elysia';
@@ -9,7 +10,6 @@ export const router = new Elysia({
 	prefix: '/authors',
 	tags: ['authors'],
 })
-	.use(superuser())
 	.get(
 		'/',
 		async ({ query: { limit, offset } }) => {
@@ -36,7 +36,7 @@ export const router = new Elysia({
 	)
 	.get(
 		'/:id',
-		async ({ set, params: { id } }) => {
+		async ({ params: { id } }) => {
 			const conn = await pool.getConnection();
 
 			const stmt = `
@@ -51,8 +51,7 @@ export const router = new Elysia({
 			const [results] = await conn.query<RowDataPacket[]>(stmt, [id]);
 			if (!results.length) {
 				conn.release();
-				set.status = 404;
-				return { message: 'Author not found' };
+				throw new HTTPError(404, 'Author not found');
 			}
 			conn.release();
 			return results;
@@ -64,106 +63,109 @@ export const router = new Elysia({
 			}),
 		},
 	)
-	.post(
-		'/',
-		async ({ set, body: { name } }) => {
-			const conn = await pool.getConnection();
-			const stmt = `
-			INSERT INTO authors
-			(	
-				id,
-				name
+	.guard((app) =>
+		app
+			.use(superuser())
+			.post(
+				'/',
+				async ({ set, body: { name } }) => {
+					const conn = await pool.getConnection();
+					const stmt = `
+					INSERT INTO authors
+					(	
+						id,
+						name
+					)
+					VALUES
+					(	
+						?,
+						?
+					);
+					`;
+					await conn.query<ResultSetHeader>(stmt, [uuidv7(), name]);
+					conn.release();
+
+					set.status = 201;
+					return { message: 'Author created' };
+				},
+				{
+					body: t.Object({
+						name: t.String({ minLength: 1, maxLength: 255 }),
+					}),
+				},
 			)
-			VALUES
-			(	
-				?,
-				?,
-				?
-			);
-			`;
-			await conn.query<ResultSetHeader>(stmt, [uuidv7(), name]);
-			conn.release();
+			.patch(
+				'/:id',
+				async ({ params: { id }, body: { name } }) => {
+					console.log(name);
+					const conn = await pool.getConnection();
+					const authorStmt = `
+					SELECT
+						*
+					FROM
+						authors
+					WHERE
+						id = ?`;
+					const [updateAuthor] = await conn.query<RowDataPacket[]>(authorStmt, [
+						id,
+					]);
+					if (!updateAuthor.length) {
+						conn.release();
+						throw new HTTPError(404, 'Author not found');
+					}
 
-			set.status = 201;
-			return { message: 'Author created' };
-		},
-		{
-			body: t.Object({
-				name: t.String({ minLength: 1, maxLength: 255 }),
-			}),
-		},
-	)
-	.patch(
-		'/:id',
-		async ({ set, params: { id }, body: { name } }) => {
-			console.log(name);
-			const conn = await pool.getConnection();
-			const authorStmt = `
-			SELECT
-				*
-			FROM
-				authors
-			WHERE
-				id = ?`;
-			const [updateAuthor] = await conn.query<RowDataPacket[]>(authorStmt, [
-				id,
-			]);
-			if (!updateAuthor.length) {
-				conn.release();
-				set.status = 404;
-				return { message: 'Author not found' };
-			}
+					// TODO: update author
 
-			// TODO: update author
+					conn.release();
 
-			conn.release();
+					return { message: 'Author updated successfully' };
+				},
+				{
+					params: t.Object({
+						id: t.String({ format: 'uuid' }),
+					}),
+					body: t.Object({
+						name: t.Optional(t.String({ minLength: 1, maxLength: 255 })),
+					}),
+				},
+			)
+			.delete(
+				'/:id',
+				async ({ set, params: { id } }) => {
+					const conn = await pool.getConnection();
 
-			return { message: 'Author updated successfully' };
-		},
-		{
-			params: t.Object({
-				id: t.String({ format: 'uuid' }),
-			}),
-			body: t.Object({
-				name: t.Optional(t.String({ minLength: 1, maxLength: 255 })),
-			}),
-		},
-	)
-	.delete(
-		'/:id',
-		async ({ set, params: { id } }) => {
-			const conn = await pool.getConnection();
+					const authorStmt = `
+					SELECT
+						*
+					FROM
+						authors
+					WHERE
+						id = ?`;
+					const [deleteAuthor] = await conn.query<RowDataPacket[]>(authorStmt, [
+						id,
+					]);
+					if (!deleteAuthor.length) {
+						conn.release();
+						throw new HTTPError(404, 'Author not found');
+					}
 
-			const authorStmt = `
-			SELECT
-				*
-			FROM
-				authors
-			WHERE
-				id = ?`;
-			const [deleteAuthor] = await conn.query<RowDataPacket[]>(authorStmt, [
-				id,
-			]);
-			if (!deleteAuthor.length) {
-				conn.release();
-				set.status = 404;
-				return { message: 'Author not found' };
-			}
+					const deleteAuthorStmt = `
+					DELETE FROM
+						authors 
+					WHERE 
+						id = ?
+					`;
+					await conn.execute<ResultSetHeader>(deleteAuthorStmt, [id]);
+					conn.release();
 
-			const deleteAuthorStmt = `
-			DELETE FROM
-				authors 
-			WHERE 
-				id = ?
-			`;
-			await conn.execute<ResultSetHeader>(deleteAuthorStmt, [id]);
-			conn.release();
-
-			return { message: 'Author deleted successfully' };
-		},
-		{
-			params: t.Object({
-				id: t.String({ format: 'uuid' }),
-			}),
-		},
+					return { message: 'Author deleted successfully' };
+				},
+				{
+					params: t.Object({
+						id: t.String({ format: 'uuid' }),
+					}),
+				},
+			),
 	);
+
+// TODO: get products by author
