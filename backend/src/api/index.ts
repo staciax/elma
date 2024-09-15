1470c5ecba99d561c6acdd86d0e54bf779f17426
STACiA <69411527+staciax@users.noreply.github.com>
2024-09-16 02:29:36 +0700

    refactor products to books

--- backend/src/api/index.ts
+++ backend/src/api/index.ts
@@ -2,9 +2,9 @@ import { Elysia } from 'elysia';
 
 import { router as authRouter } from './routes/auth';
 import { router as authorsRouter } from './routes/authors';
+import { router as booksRouter } from './routes/books';
 import { router as categoriesRouter } from './routes/categories';
 import { router as ordersRouter } from './routes/orders';
-import { router as booksRouter } from './routes/books';
 import { router as publishersRouter } from './routes/publishers';
 import { router as shoppingCartsRouter } from './routes/shopping-carts';
 import { router as usersRouter } from './routes/users';
