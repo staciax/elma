1de726415106a0de0c758a709252f35ea16b8452
STACiA <69411527+staciax@users.noreply.github.com>
2024-09-17 02:20:01 +0700

    add actions for authors, categories, publishers

--- apps/frontend/lib/elma/actions/authors.ts
+++ apps/frontend/lib/elma/actions/authors.ts
@@ -1,1 +1,28 @@
 'use server';
+
+import axios from '@/lib/axios';
+
+import type { AuthorPublic, AuthorsPublic } from '../types/authors';
+
+export const getAuthor = async (id: string): Promise<AuthorPublic[]> => {
+	try {
+		const response = await axios.get(`/v1/authors/${id}/`);
+		return response.data;
+	} catch (error) {
+		return Promise.reject(error);
+	}
+};
+
+export const getAuthors = async (options?: {
+	limit?: number;
+	offset?: number;
+}): Promise<AuthorsPublic> => {
+	try {
+		const response = await axios.get('/v1/authors/', {
+			params: options,
+		});
+		return response.data;
+	} catch (error) {
+		return Promise.reject(error);
+	}
+};
--- apps/frontend/lib/elma/actions/categories.ts
+++ apps/frontend/lib/elma/actions/categories.ts
@@ -1,1 +1,29 @@
 'use server';
+
+import axios from '@/lib/axios';
+
+import type { CategoriesPublic, CategoryPublic } from '../types/categories';
+
+export const getCategory = async (id: string): Promise<CategoryPublic[]> => {
+	console.log('product id:', id);
+	try {
+		const response = await axios.get(`/v1/categories/${id}/`);
+		return response.data;
+	} catch (error) {
+		return Promise.reject(error);
+	}
+};
+
+export const getCategories = async (options?: {
+	limit?: number;
+	offset?: number;
+}): Promise<CategoriesPublic> => {
+	try {
+		const response = await axios.get('/v1/categories/', {
+			params: options,
+		});
+		return response.data;
+	} catch (error) {
+		return Promise.reject(error);
+	}
+};
