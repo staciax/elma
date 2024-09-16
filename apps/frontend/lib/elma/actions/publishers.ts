02a65745f721dcfea6270c10ef028841396bd46f
STACiA <69411527+staciax@users.noreply.github.com>
2024-09-17 02:20:01 +0700

    add actions for authors, categories, publishers

--- apps/frontend/lib/elma/actions/publishers.ts
+++ apps/frontend/lib/elma/actions/publishers.ts
@@ -1,1 +1,28 @@
 'use server';
+
+import axios from '@/lib/axios';
+
+import type { PublisherPublic, PublishersPublic } from '../types/publishers';
+
+export const getPublisher = async (id: string): Promise<PublisherPublic[]> => {
+	try {
+		const response = await axios.get(`/v1/publishers/${id}/`);
+		return response.data;
+	} catch (error) {
+		return Promise.reject(error);
+	}
+};
+
+export const getPublishers = async (options?: {
+	limit?: number;
+	offset?: number;
+}): Promise<PublishersPublic> => {
+	try {
+		const response = await axios.get('/v1/publishers/', {
+			params: options,
+		});
+		return response.data;
+	} catch (error) {
+		return Promise.reject(error);
+	}
+};
