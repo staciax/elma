72b0ca6f353dbe31c109b0cc44de7ba002eebf36
STACiA <69411527+staciax@users.noreply.github.com>
2024-09-17 02:20:36 +0700

    add action user get all

--- apps/frontend/lib/elma/actions/users.ts
+++ apps/frontend/lib/elma/actions/users.ts
@@ -1,11 +1,20 @@
 'use server';
 
 import axios from '@/lib/axios';
-import type { UserPublic } from '../types';
+import type { UserPublic, UsersPublic } from '../types';
 
 export const getUserMe = async (): Promise<UserPublic> => {
 	try {
 		const response = await axios.get('/v1/users/me');
+		return response.data;
+	} catch (error) {
+		return Promise.reject(error);
+	}
+};
+
+export const getUsers = async (): Promise<UsersPublic> => {
+	try {
+		const response = await axios.get('/v1/users');
 		return response.data;
 	} catch (error) {
 		return Promise.reject(error);
