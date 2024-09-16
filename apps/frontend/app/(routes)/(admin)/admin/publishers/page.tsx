d5402d231e6132d4ee292dd6783ee871ea2c8530
STACiA <69411527+staciax@users.noreply.github.com>
2024-09-17 02:18:37 +0700

    add, update admin page for authors, publishers, users, books

---
+++ apps/frontend/app/(routes)/(admin)/admin/publishers/page.tsx
@@ -0,0 +1,117 @@
+import { Button } from '@/components/ui/button';
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from '@/components/ui/card';
+import {
+	DropdownMenu,
+	DropdownMenuCheckboxItem,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from '@/components/ui/table';
+import { File, ListFilter, MoreHorizontal, PlusCircle } from 'lucide-react';
+
+import { getPublishers } from '@/lib/elma/actions/publishers';
+
+export default async function Page() {
+	const result = await getPublishers();
+	return (
+		<main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-6">
+			<div className="ml-auto flex items-center gap-2">
+				<DropdownMenu>
+					<DropdownMenuTrigger asChild>
+						<Button variant="outline" size="sm" className="h-8 gap-1">
+							<ListFilter className="h-3.5 w-3.5" />
+							<span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+								จัดเรียง
+							</span>
+						</Button>
+					</DropdownMenuTrigger>
+					<DropdownMenuContent align="end">
+						<DropdownMenuLabel>จัดเรียง โดย</DropdownMenuLabel>
+						<DropdownMenuSeparator />
+						<DropdownMenuCheckboxItem checked>ชื่อ</DropdownMenuCheckboxItem>
+						<DropdownMenuCheckboxItem>ที่อยู่</DropdownMenuCheckboxItem>
+					</DropdownMenuContent>
+				</DropdownMenu>
+				<Button size="sm" className="h-8 gap-1">
+					<PlusCircle className="h-3.5 w-3.5" />
+					<span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+						เพิ่มสำนักพิมพ์
+					</span>
+				</Button>
+			</div>
+			<Card x-chunk="dashboard-06-chunk-0">
+				<CardHeader>
+					<CardTitle>สำนักพิมพ์</CardTitle>
+					<CardDescription>จัดการสำนักพิมพ์</CardDescription>
+				</CardHeader>
+				<CardContent>
+					<Table>
+						<TableHeader>
+							<TableRow>
+								<TableHead>ชื่อ</TableHead>
+								<TableHead className="hidden md:table-cell">ที่อยู่</TableHead>
+								<TableHead>
+									<span className="sr-only">Actions</span>
+								</TableHead>
+							</TableRow>
+						</TableHeader>
+						<TableBody>
+							{result.data.map((publisher) => (
+								<TableRow key={publisher.id}>
+									<TableCell className="font-medium">
+										{publisher.name}
+									</TableCell>
+									<TableCell className="hidden md:table-cell">
+										publisher.description
+									</TableCell>
+									<TableCell>
+										<DropdownMenu>
+											<DropdownMenuTrigger asChild>
+												<Button
+													aria-haspopup="true"
+													size="icon"
+													variant="ghost"
+												>
+													<MoreHorizontal className="h-4 w-4" />
+													<span className="sr-only">Toggle menu</span>
+												</Button>
+											</DropdownMenuTrigger>
+											<DropdownMenuContent align="end">
+												<DropdownMenuLabel>Actions</DropdownMenuLabel>
+												<DropdownMenuItem>Edit</DropdownMenuItem>
+												<DropdownMenuItem>Delete</DropdownMenuItem>
+											</DropdownMenuContent>
+										</DropdownMenu>
+									</TableCell>
+								</TableRow>
+							))}
+						</TableBody>
+					</Table>
+				</CardContent>
+				<CardFooter>
+					<div className="text-muted-foreground text-xs">
+						Showing <strong>1-10</strong> of <strong>{result.count}</strong>{' '}
+						products
+					</div>
+				</CardFooter>
+			</Card>
+		</main>
+	);
+}
