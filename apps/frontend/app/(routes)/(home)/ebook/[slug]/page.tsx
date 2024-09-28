05634a06e172b534a4cab8238f71c9f9dcac4977
STACiA <69411527+staciax@users.noreply.github.com>
2024-09-28 17:52:32 +0700

    ad suspense for book detail

--- apps/frontend/app/(routes)/(home)/ebook/[slug]/page.tsx
+++ apps/frontend/app/(routes)/(home)/ebook/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import BookDetail from '@/components/books/book-detail';
 import { getBook } from '@/lib/elma/actions/books';
 import { notFound } from 'next/navigation';
+import { Suspense } from 'react';
 
 // TODO: use decimal.js for price calculation
 // https://github.com/MikeMcl/decimal.js
@@ -8,19 +9,19 @@ import { notFound } from 'next/navigation';
 
 // TODO: remove duplicate code
 
-// export async function generateMetadata({ params }: Props) {
-// 	const slug = params.slug;
+export async function generateMetadata({ params }: Props) {
+	const slug = params.slug;
 
-// 	const results = await getProduct(slug);
-// 	if (!results.length) {
-// 		return <div>Product not found</div>;
-// 	}
-// 	const product = results[0];
+	const results = await getBook(slug);
+	if (!results) {
+		return null;
+	}
+	const product = results[0];
 
-// 	return {
-// 		title: product.product_title,
-// 	};
-// }
+	return {
+		title: product.title,
+	};
+}
 
 type Props = {
 	params: { slug: string };
@@ -37,7 +38,9 @@ export default async function Page({ params }: Props) {
 
 	return (
 		<main className="container flex flex-col items-center">
-			<BookDetail book={product} />
+			<Suspense fallback={<div>Loading...</div>}>
+				<BookDetail book={product} />
+			</Suspense>
 			<section>TEST</section>
 		</main>
 	);
