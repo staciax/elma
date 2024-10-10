d4143b7931cc3f5ef4e78f511040e6d5c88a342f
STACiA <69411527+staciax@users.noreply.github.com>
2024-10-10 23:41:46 +0700

    feat: extend UserRow type to include hashed_password

--- apps/backend/src/types/users.ts
+++ apps/backend/src/types/users.ts
@@ -2,4 +2,7 @@ import type { UserPublic } from '@/schemas/users';
 import type { UnwrapSchema } from 'elysia';
 import type { RowDataPacket } from 'mysql2';
 
-export type UserRow = RowDataPacket & UnwrapSchema<typeof UserPublic>;
+export type UserRow = RowDataPacket &
+	(UnwrapSchema<typeof UserPublic> & {
+		hashed_password: string;
+	});
